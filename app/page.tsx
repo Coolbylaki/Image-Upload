4a03305f7430a1916982f57cdd8963b186e44993
Lazar Stojanović <lakisd98@gmail.com>
2023-07-13 01:30:41 +0200

    Update: card styling finish & typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
 	return (
 		<section className={styles.card}>
 			<h1 className={styles.title}>Upload your image</h1>
 			<p className={styles.subtitle}>File should be Jpeg, Png...</p>
+			<div className={styles["img-box"]}>
+				<Image src={"./mountain.svg"} alt="Mountain image" width={115} height={90} />
+				<p>Drag & Drop your image here</p>
+			</div>
 			<p>Or</p>
+
 			<button className={styles.btn} type="button">
 				Choose a file
 			</button>
