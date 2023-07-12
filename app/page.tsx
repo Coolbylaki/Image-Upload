e56a4f58b291eb2a4dd41b2f96782cd02b939f5a
Lazar Stojanović <lakisd98@gmail.com>
2023-07-13 01:13:33 +0200

    Update: card styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import styles from "./page.module.css";
 export default function Home() {
 	return (
 		<section className={styles.card}>
-			<span>Hello</span>
-			<span>Hello</span>
+			<h1 className={styles.title}>Upload your image</h1>
+			<p className={styles.subtitle}>File should be Jpeg, Png...</p>
+			<p>Or</p>
+			<button className={styles.btn} type="button">
+				Choose a file
+			</button>
 		</section>
 	);
 }
