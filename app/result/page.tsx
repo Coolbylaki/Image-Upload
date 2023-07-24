a4cffa996e108fe4ac7b64a8ce849a291577f678
Lazar Stojanović <lakisd98@gmail.com>
2023-07-25 01:22:12 +0200

    Add: result page functionality to display image

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -4,6 +4,11 @@ import { useImageContext } from "@/store/ImageContext";
 import { useEffect, useState } from "react";
 import { getDownloadURL } from "firebase/storage";
 
+import Image from "next/image";
+import checkSvg from "@/assets/check.svg";
+
+import styles from "./page.module.css";
+
 export default function Result() {
 	const [imageUrl, setImageUrl] = useState("");
 	const { imageRef } = useImageContext();
@@ -20,8 +25,22 @@ export default function Result() {
 	});
 
 	return (
-		<>
-			<h1>Hello</h1>
-		</>
+		<section className={styles.card}>
+			<Image src={checkSvg} alt="Successful" />
+			<h1 className={styles.title}>Uploaded Successfully!</h1>
+			<Image
+				src={imageUrl}
+				alt="Your image"
+				width={338}
+				height={224}
+				className={styles.img}
+			/>
+			<div className={styles.img__link}>
+				<h2>{imageUrl}</h2>
+				<button className={styles.img__btn}>
+					<p>Copy</p>
+				</button>
+			</div>
+		</section>
 	);
 }
